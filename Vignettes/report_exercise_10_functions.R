1953d73def27c5776e2c64164df9a38c5e0aae14
Elias Koeppel <elias.koeppel@hotmail.com>
2023-05-26 17:52:33 +0200

    I changed the eval_model fnction so that I only get the plot of the test data.

--- Vignettes/report_exercise_10_functions.R
+++ Vignettes/report_exercise_10_functions.R
@@ -81,33 +81,24 @@ get_pp <- function(data){
 
 
 # model evaluation 
-eval_model <- function(mod, df_train, df_test){
+eval_model <- function(mod, df_test){
   
   # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
+
   
-  df_train$fitted <- stats::predict(mod, newdata = df_train)
   
   df_test <- df_test |> 
     drop_na()
   df_test$fitted <- stats::predict(mod, newdata = df_test)
   
   # get metrics tables
-  metrics_train <- df_train |> 
-    yardstick::metrics(GPP_NT_VUT_REF, fitted)
+
   
   metrics_test <- df_test |> 
     yardstick::metrics(GPP_NT_VUT_REF, fitted)
   
   # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
+
   rmse_test <- metrics_test |> 
     filter(.metric == "rmse") |> 
     pull(.estimate)
@@ -117,14 +108,7 @@ eval_model <- function(mod, df_train, df_test){
   
   # visualise as a scatterplot
   # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
+ 
   
   plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
     geom_point(alpha = 0.3) +
@@ -135,9 +119,9 @@ eval_model <- function(mod, df_train, df_test){
          title = "Test set") +
     theme_classic()
   
-  out <- cowplot::plot_grid(plot_1, plot_2)
+
   
-  return(out)
+  return(plot_2)
 }
 
 
